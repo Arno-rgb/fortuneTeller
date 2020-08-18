4011979a1f6adaebd26f4d8088c095b8fd6b1cd8
Arno Meiring <arnomeiring3@gmail.com>
2020-08-18 09:38:05 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace fortuneTeller
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnTruth = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.DodgerBlue;
+            this.textBox1.Font = new System.Drawing.Font("Franklin Gothic Medium", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.Cyan;
+            this.textBox1.Location = new System.Drawing.Point(12, 348);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(776, 37);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "What secrets to the future can Arno\'s Program show you today?";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.textBox1_MouseClick);
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.Enter += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // btnTruth
+            // 
+            this.btnTruth.BackColor = System.Drawing.Color.GreenYellow;
+            this.btnTruth.Font = new System.Drawing.Font("Russo One", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTruth.Location = new System.Drawing.Point(308, 391);
+            this.btnTruth.Name = "btnTruth";
+            this.btnTruth.Size = new System.Drawing.Size(187, 59);
+            this.btnTruth.TabIndex = 1;
+            this.btnTruth.Text = "THE TRUTH";
+            this.btnTruth.UseVisualStyleBackColor = false;
+            this.btnTruth.Click += new System.EventHandler(this.btnTruth_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Bowlby One SC", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FloralWhite;
+            this.label1.Location = new System.Drawing.Point(472, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(316, 137);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ask any question and get a answer but you might not like what you see.......\r\n";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Peru;
+            this.BackgroundImage = global::fortuneTeller.Properties.Resources.monkey;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnTruth);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Arno\'s fortune teller";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnTruth;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fortuneTeller
+{
+    public partial class Form1 : Form
+    {
+        System.Media.SoundPlayer theme = new System.Media.SoundPlayer(@"0731.wav");
+        public Form1()
+        {
+            InitializeComponent();
+
+            theme.Play();
+        }
+
+        private void btnTruth_Click(object sender, EventArgs e)
+        {
+            
+            Random rand = new Random();
+            var random = rand.Next(1, 15);
+            if (random <= 1)
+            {
+                textBox1.Text = "No chance in hell!!!";
+            }
+            else if (random <= 2)
+            {
+                textBox1.Text = "Very likely.";
+            }
+            else if (random <= 3)
+            {
+                textBox1.Text = "You can only hope.";
+            }
+            else if (random <= 4)
+            {
+                textBox1.Text = "Looks like it.";
+            }
+            else if (random <= 5)
+            {
+                textBox1.Text = "No chance";
+            }
+            else if (random <= 6)
+            {
+                textBox1.Text = "Very unlikely";
+            }
+            else if (random <= 7)
+            {
+                textBox1.Text = "Yes Baby .";
+            }
+            else if (random <= 8)
+            {
+                textBox1.Text = "Offcourse...";
+            }
+            else if (random <= 9)
+            {
+                textBox1.Text = "Sarcastic Yes!!";
+            }
+            else if (random <= 10)
+            {
+                textBox1.Text = "Difficult to say...you probably know the answer already...";
+            }
+            else if (random <= 11)
+            {
+                textBox1.Text = "Better believe it";
+            }
+            else if (random <= 12)
+            {
+                textBox1.Text = "Your mama said so...";
+            }
+            else if (random <= 13)
+            {
+                textBox1.Text = "Well... maybe";
+            }
+            else
+            {
+                textBox1.Text = "Glitch in the Matrix try again.";
+                {
+                    return;
+                }
+            }
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            Console.ReadLine();
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            textBox1.MaxLength = 100;
+            textBox1.Text = textBox1.Text;
+        }
+
+        private void textBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            textBox1.Clear();
+        }
+    }
+}
